46516324437a4caa471afbed9412d25cc4f42a7b
emidinho <emidioalemnju@yahoo.com>
2023-04-15 21:31:56 +0800

    update

---
+++ Data types-Variables-Operators-Keywords-Comments-Type conversion/hands-on.py
@@ -0,0 +1,18 @@
+# print(181500)
+# print(59*125)
+# price = 15 
+# quantity = 10
+
+# print (price*quantity)
+
+
+# print("emidio")
+# print("learning pytho")
+
+# print (type ("learning pytho"))
+
+True
+False
+
+Civil_status = None
+print(Civil_status, type(None))
