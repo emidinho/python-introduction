a121556e9c99ea0a4a9cf60ba143e1d1f333051b
emidinho <you@example.com>
2023-04-22 05:33:21 +0800

    update

--- Loops/hands-on.py
+++ Loops/hands-on.py
@@ -1,3 +1,5 @@
+### for-loop (used for sequences)
+
 # name = "pytho workshop"
 
 # for each_character in name:
@@ -5,26 +7,90 @@
 
 
 # fruits = ["apple", "orange", "grapes", "mangoes", "lemons"]
+# for fruit_types in fruits:
+#     print(fruit_types)
 
 
 
-marks = [63,77, 23,97,98,85,95,80, 87,93]
-print("\n grading system ")
+# marks = [63,77, 23,97,98,85,95,80, 87,93]
+# print("\n grading system ")
 
-for x in marks:
-    print(x)
-    if x >= 75 and x <= 80:
-        print("Passed: C")
-    elif x >= 81 and x <= 85:
-        print("Passed: B-")
-    elif x >= 86 and x <= 90:
-        print("Passed: B")
-    elif x >= 91 and x <= 94:
-        print("Passed: B+")
-    elif x >= 95 and x <= 97:
-        print("Passed: A")
-    elif x >= 98 and x <= 100:
-        print("Passed: A+")    
-    else:
-        print("Failed")
+# for x in marks:
+#     print(x)
+#     if x >= 75 and x <= 80:
+#         print("Passed: C")
+#     elif x >= 81 and x <= 85:
+#         print("Passed: B-")
+#     elif x >= 86 and x <= 90:
+#         print("Passed: B")
+#     elif x >= 91 and x <= 94:
+#         print("Passed: B+")
+#     elif x >= 95 and x <= 97:
+#         print("Passed: A")
+#     elif x >= 98 and x <= 100:
+#         print("Passed: A+")    
+#     else:
+#         print("Failed")
+
+# buckets = ["invoice-bucket", "images-bucket", "documents-bucket", "movies-bucket"]
+
+# for bucket in buckets:
+#     print(bucket)
+
+# for i in range(1,50,10):
+#     print(i)
+
+# for i in range(5):
+#     print("enter pin")
+
+# student = {"name": "emidio", "school": "GSN", "place": "fontem", "marital-status": "single", 
+#            "employment-status": "unemployed"}
+# for i,j in student.items():
+#     print(i,j)
+
+### for loop.
+
+
+### while loop.(used for conditions)
+# pin_count = 0
+
+# while pin_count <= 5:
+#     pin_entered = False
+#     if pin_entered == False:
+#         pin_count = pin_count + 1
+#         if pin_count <= 5:
+#             print("please enter your pin again")
+#         else:
+#             print("pin block, try after 1 hour")
+
+# count = 1
+
+# while count < 10:
+#     print (count)
+#     count = count + 1
+
+### while loop.
+
+
+### nested loops
+
+student = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+for i in student:
+    print(i)
+    for j in i:
+        print(j)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
