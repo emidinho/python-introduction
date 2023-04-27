ac5ac04ce0f551c749022046dadc58e3208f6be6
emidinho <you@example.com>
2023-04-27 22:00:42 +0800

    updates

--- Functions/hands-on.py
+++ Functions/hands-on.py
@@ -16,16 +16,63 @@
 # sum (4000, 4895)
 
 
-def check_balance(account_number):
-    print("current account:", account_number, "current balance:", 1000)
+# def check_balance(account_number):
+#     print("current account:", account_number, "current balance:", 1000)
 
 
-check_balance(145218569814587)
+# check_balance(145218569814587)
 
 
-def send_money(amount, receivers_account, account_number):
-    current_balance = check_balance(account_number)
-    print("receivers account: ", receivers_account, "senders account: ", account_number, "amount transfered: ", amount)
+# def send_money(amount, receivers_account, account_number):
+#     current_balance = check_balance(account_number)
+#     print("receivers account: ", receivers_account, "senders account: ", account_number, "amount transfered: ", amount)
 
 
-send_money(125463, 145218569814587, 1500)
+# send_money(125463, 145218569814587, 1500)
+
+
+
+# def calculate_bmi (weight, height):
+#     bmi = weight/(height*height)
+#     if bmi < 18.5:
+#         print("bmi:", bmi,"underweight")
+#     elif bmi >= 18.5 and bmi <= 24.9:
+#         print ("bmi:", bmi,"normal")
+#     elif bmi >= 25.0 and bmi <= 29.9:
+#         print("bmi:",bmi,"overweight")
+#     elif bmi >= 30.0 and bmi <= 34.9:
+#         print("bmi:",bmi,"obesity: 1st class")
+#     elif bmi >= 35.0 and bmi <= 39.9:
+#         print("bmi:",bmi,"obesity: 2nd class")
+#     else:
+#         print("bmi:",bmi,"obesity: 3rd class")
+
+
+# calculate_bmi(60, 1.79)
+# calculate_bmi(80, 1.59)
+# calculate_bmi(40, 1.20)
+# calculate_bmi(50, 1.60)
+# calculate_bmi(40, 1.75)
+
+# def Male_craetinine_clearance(age, weight, serum_creatinine):
+#     creatinine_clearance = ((140-age)*weight)/(72*serum_creatinine)
+#     if creatinine_clearance < 107:
+#         print("creatinine clearance:",creatinine_clearance,"below normal range; abnormal renal function")
+#     elif creatinine_clearance >= 107 and creatinine_clearance <= 139:
+#         print("creatinine clearance:",creatinine_clearance,"within normal rangenormal renal function")
+#     else:
+#         print("creatinine clearance:",creatinine_clearance,"above normal range; abnormal renal function")
+
+# def Female_creatininie_clearance(age, weight, serum_creatinine):
+#     creatinine_clearance = (((140-age)*weight)/(72*serum_creatinine))*0.85
+#     if creatinine_clearance < 87:
+#         print("creatinine clearance:",creatinine_clearance,"below normal range; abnormal renal function")
+#     elif creatinine_clearance >= 87 and creatinine_clearance <= 107:
+#         print("creatinine clearance:",creatinine_clearance,"within normal rangenormal renal function")
+#     else:
+#         print("creatinine clearance:",creatinine_clearance,"above normal range; abnormal renal function")
+
+
+# Female_creatininie_clearance(60, 61.235, 3.5)
+# Male_craetinine_clearance(60, 61.235, 3.5)
+
