bec59237efaba690be123e3650dc04d2149e6b56
emidinho <emidioalemnju@yahoo.com>
2023-05-13 02:57:50 +0800

    update

--- boto3-learning/terminate-all-instances.py
+++ boto3-learning/terminate-all-instances.py
@@ -2,7 +2,7 @@ import boto3
 
 session = boto3.session.Session(profile_name="default", region_name="us-east-1")
 
-ec2 = boto3.resource('ec2')
+ec2 = session.resource('ec2')
 
 #get only running instances
 instances = ec2.instances.filter(
