baf87706d95bec3083c1ad47936ff416cb760960
emidinho <emidioalemnju@yahoo.com>
2023-07-26 11:00:12 +0800

    update

---
+++ Hands-On/list.py
@@ -0,0 +1,49 @@
+# fruits = ["oranges","grapes","mangoes","lemons","tangerines","banana"]
+
+# students = ["james","jake","sony","bob","virj","mariia"]
+
+# speeds = [0,20,40,60,80,100]
+
+# custom_list = ["emidio",15,50.4,True,None]
+
+# print(students)
+# print("lemons" in fruits)
+# print(speeds)
+# print(custom_list)
+
+# print(type(students))
+# print(type(fruits))
+# print(type(speeds))
+# print(type(custom_list))
+
+# print(len(students))
+# print(len(fruits))
+# print(len(speeds))
+# print(len(custom_list))
+
+# print(fruits[0])
+# print(fruits[0:])
+# print(fruits[0:3])
+# print(students[1])
+# print(students[1:])
+# print(students[1:4])
+
+# fruits[4] = "limes"
+# print(fruits[0:])
+
+# print(dir(fruits))
+
+# students.reverse()
+# print(students)
+
+fruits = ["kiwi", "grapes", "apple", "orange", "banana", "strawberry", "watermelon", "pineapple", "mango", "pear"]
+
+
+print(fruits)
+print(fruits.sort(reverse=True))                 
+print(fruits.reverse())  
+print(fruits.append("zanziba"))
+print(fruits.index("mango"))
+
+for each_fruit in fruits:
+    print(each_fruit)
