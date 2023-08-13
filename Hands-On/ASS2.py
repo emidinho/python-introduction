20762b8308dc6981d2b2314f9f750f867836be0e
emidinho <emidioalemnju@yahoo.com>
2023-08-14 02:38:44 +0800

    update

--- Hands-On/ASS2.py
+++ Hands-On/ASS2.py
@@ -1,2 +1,17 @@
 # Prof: I have a good usecase.
 # Create a list of names and friends, and then iterate and get them all greeted
+
+
+friends = ["ewane", "samba", "confy", "marian", "ansible", "paul", "eman", "ralph", "chan", "mariia"]
+
+def greetings_for_friends():
+    for friend in friends:
+        print("hello dearest", friend,"how's the life so far?")
+        print("it's been ages since we last talked")
+        print("hopefully we meet this summer in paris for some catching up")
+        print("i'll be in france during the summer for a conference.")
+        print()
+
+
+
+greetings_for_friends()
